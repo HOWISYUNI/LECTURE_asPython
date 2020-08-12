93048faf38081f065038a01d577efaf7fbddf8d2
HOWISYUNI <54610651+HOWISYUNI@users.noreply.github.com>
2020-08-12 16:46:46 +0900

    Update 2차원리스트뒤집기.py
    zip, map함수의 리턴값에 대한 주석 수정

--- correct solution/2차원리스트뒤집기.py
+++ correct solution/2차원리스트뒤집기.py
@@ -18,6 +18,8 @@ mylist = [ [1,2,3], [4,5,6], [7,8,9] ]
 # '*iterables'은 여러개의 iterable(반복가능객체)로 생각할것. zip(list1, list2, list3)와 같이 unpacking이 아니라 여러개 리스트를 전달하기도한다.
 new_list = list(map(list,zip(*mylist)))
 # *mylist의 결과  [1,2,3], [4,5,6], [7,8,9]  가 리턴된다
-# zip함수를 씌우면 ((1,4,7), (2,5,8), (3,6,9)) 튜플 iterator가 리턴된다. zip()함수는 tuple을 리턴한다.
-# map 함수를 이용해 각 튜플에 list함수를 적용시킨다. [[1,4,7], [2,5,8], [3,6,9]]의 iterator들이 리턴된다. map()함수는 list를 리턴한다.
+# zip함수를 씌우면 (1,4,7) (2,5,8) (3,6,9) 튜플 iterator가 리턴된다. zip()함수는 zip object를 리턴한다.
+# map 함수를 이용해 각 튜플에 list함수를 적용시킨다. [1,4,7] [2,5,8] [3,6,9]의 iterator가 리턴된다. map()함수는 map object를 리턴한다. map object는 iterator이다(https://dojang.io/mod/page/view.php?id=2286)
 # list함수로 iterator들을 list로 만든다. [[1,4,7], [2,5,8], [3,6,9]]
+
+# 현재 정확히는 알 수 없지만, map(), zip() 함수 결과는 iterable이 아니라 iterator이다. 따라서 iterable로 만들기 위해 list()나 dict()등으로 확실하게 iterable로 만들어야한다.(https://dojang.io/mod/page/view.php?id=2405 의 그림 39-2)
